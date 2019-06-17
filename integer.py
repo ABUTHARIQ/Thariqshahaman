09bf72ac84d996089b434a14350dc700bb81adbc
ABUTHARIQ <42167193+ABUTHARIQ@users.noreply.github.com>
2019-06-17 12:13:06 +0530

    Update integer.py

--- integer.py
+++ integer.py
@@ -1,6 +1,6 @@
-N,K=map(int,input().split())
-t=list(map(int,input().split()))
+M,L=map(int,input().split())
+p=list(map(int,input().split()))
 sum=0
-for i in range(0,K):
- sum=sum+t[i]
+for i in range(0,L):
+ sum=sum+p[i]
 print(sum)
